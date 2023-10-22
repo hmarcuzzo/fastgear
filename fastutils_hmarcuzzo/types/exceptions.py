97bdfe7f5a5c36533bc5ef75f8640fb0ba92db8d
hmarcuzzo <henrique.souza.m06@gmail.com>
2023-10-23 00:40:50 +0100

    Refactor exceptions, base_exception and rename modules
    The BaseException has been refactored to CustomBaseException to give a clear identity. All exceptions now inherit from CustomBaseException for a unified handling process. Additionally, the folder structure and file names for exceptions, find options, delete result, update result and utilities have been renamed and moved for more clarity and better organization of the code.

--- fastutils_hmarcuzzo/types/exceptions.py
+++ fastutils_hmarcuzzo/types/exceptions.py
@@ -7,28 +7,28 @@ from starlette.status import (
     HTTP_404_NOT_FOUND,
 )
 
-from fastutils_hmarcuzzo.common.base_exception import BaseExceptionType
+from fastutils_hmarcuzzo.types.custom_base_exception import CustomBaseException
 
 
-class BadRequestException(BaseExceptionType):
+class BadRequestException(CustomBaseException):
     def __init__(self, msg: str, loc: List[str] = [], exception_type: str = "Bad Request"):
         self.status_code = HTTP_400_BAD_REQUEST
         super().__init__(msg, loc, exception_type)
 
 
-class UnauthorizedException(BaseExceptionType):
+class UnauthorizedException(CustomBaseException):
     def __init__(self, msg: str, loc: List[str] = [], _type: str = "unauthorized"):
         self.status_code = HTTP_401_UNAUTHORIZED
         super().__init__(msg, loc, _type)
 
 
-class ForbiddenException(BaseExceptionType):
+class ForbiddenException(CustomBaseException):
     def __init__(self, msg: str, loc: List[str] = [], _type: str = "forbidden"):
         self.status_code = HTTP_403_FORBIDDEN
         super().__init__(msg, loc, _type)
 
 
-class NotFoundException(BaseExceptionType):
+class NotFoundException(CustomBaseException):
     def __init__(self, msg: str, loc: List[str] = [], exception_type: str = "Not Found"):
         self.status_code = HTTP_404_NOT_FOUND
         super().__init__(msg, loc, exception_type)

969043104867eb4adaf789a53285511ae3172015
hmarcuzzo <henrique.souza.m06@gmail.com>
2023-11-26 15:43:39 +0000

    feat: Add search and sort to Pagination, refactor custom_pages
    Enhanced the Pagination TypedDict in pagination.py by adding sort and search classes, allowing for a more detailed pagination functionality. Also refactored the 'custom_pages.py' to handle default values for pagination, improving clarity and cleanliness of the code. The update introduces search and sort capabilities in the pagination, thus increasing the functionality of our web applications.

--- fastutils_hmarcuzzo/types/pagination.py
+++ fastutils_hmarcuzzo/types/pagination.py
@@ -1,6 +1,18 @@
-from typing import TypedDict
+from typing import TypedDict, List
+
+
+class PaginationSearch(TypedDict):
+    field: str
+    value: str
+
+
+class PaginationSort(TypedDict):
+    field: str
+    by: str
 
 
 class Pagination(TypedDict):
     skip: int
     take: int
+    sort: List[PaginationSort]
+    search: List[PaginationSearch]

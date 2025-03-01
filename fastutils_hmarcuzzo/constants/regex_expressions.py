0da1edc5e235a59e05ac92523add4a698b60f7cd
Henrique Marcuzzo <hennique.souza.m06@gmail.com>
2025-03-01 18:00:41 +0000

    refactor: add regex patterns for ORDER BY clause, logical operators, and any character matching

--- fastutils_hmarcuzzo/constants/regex_expressions.py
+++ fastutils_hmarcuzzo/constants/regex_expressions.py
@@ -1,2 +1,8 @@
+# Regular expression to match an ORDER BY clause with 'ASC' or 'DESC'.
 REGEX_ORDER_BY_QUERY = "(ASC|DESC)$"
-REGEX_ALPHANUMERIC_WITH_UNDERSCORE = "[a-zA-Z0-9_]+"
+
+# Regular expression to match any character (zero or more times).
+REGEX_ANY_CHAR = ".*"
+
+# Regular expression to match logical operators 'AND' or 'OR'.
+REGEX_OPERATOR = "(AND|OR)$"

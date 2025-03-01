78fed1232dc5fef8587acf8bf7c5f73581123016
Henrique Marcuzzo <hennique.souza.m06@gmail.com>
2025-03-01 18:03:16 +0000

    feat: enhance database session management with synchronous and asynchronous factories

--- fastutils_hmarcuzzo/common/database/sqlalchemy/base_entity.py
+++ fastutils_hmarcuzzo/common/database/sqlalchemy/base_entity.py
@@ -1,11 +1,14 @@
 import re
-import uuid
 from dataclasses import dataclass
 from datetime import datetime
+from uuid import UUID
 
-from sqlalchemy import Column, DateTime, event
+from sqlalchemy import DateTime, event
 from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import Mapped
+from sqlalchemy.testing.schema import mapped_column
 from sqlalchemy_utils import UUIDType
+from uuid6 import uuid7
 
 from fastutils_hmarcuzzo.common.database.sqlalchemy.base import Base
 
@@ -14,30 +17,47 @@ from fastutils_hmarcuzzo.common.database.sqlalchemy.base import Base
 class BaseEntity(Base):
     __abstract__ = True
 
-    id = Column(UUIDType(binary=False), primary_key=True, default=uuid.uuid4)
-    created_at = Column(DateTime(timezone=True), nullable=False)
-    updated_at = Column(DateTime(timezone=True), nullable=False)
+    id: Mapped[UUID] = mapped_column(UUIDType(), primary_key=True, default=uuid7)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     @declared_attr
     def __tablename__(self) -> str:
-        """
-            This method is used to generate the name of the table that will be created in the database for a given BaseEntity class.
+        """Generates the name of the table that will be created in the database for a given BaseEntity class.
+
         The table name is derived from the class name by converting it from CamelCase to snake_case.
+
+        Returns:
+            str: The generated table name in snake_case.
+
         """
         return "_".join(re.findall("[A-Z][^A-Z]*", self.__name__)).lower()
 
 
 @event.listens_for(BaseEntity, "before_insert", propagate=True)
 def set_before_insert(mapper, connection, target: BaseEntity) -> None:
+    """Event listener that sets the `created_at` and `updated_at` timestamps before inserting a new record.
+
+    This function is triggered before a new `BaseEntity` record is inserted into the database.
+
+    It ensures that the `created_at` and `updated_at` fields are set to the current datetime if they are not already
+    set.
+
+    Args:
+        mapper: The SQLAlchemy mapper.
+        connection: The database connection.
+        target (BaseEntity): The instance of `BaseEntity` being inserted.
+
+    """
     if not target.created_at:
-        target.created_at = datetime.utcnow()
+        target.created_at = datetime.now()
     if not target.updated_at or target.updated_at < target.created_at:
         target.updated_at = target.created_at
 
 
 @event.listens_for(BaseEntity, "before_update", propagate=True)
 def set_before_update(mapper, connection, target: BaseEntity) -> None:
-    target.updated_at = datetime.utcnow()
+    target.updated_at = datetime.now()
--- fastutils_hmarcuzzo/common/database/sqlalchemy/session.py
+++ fastutils_hmarcuzzo/common/database/sqlalchemy/session.py
@@ -1,11 +1,69 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    AsyncSessionTransaction,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import Session, SessionTransaction, sessionmaker
+
+SyncSessionType = Session | SessionTransaction
+AsyncSessionType = AsyncSession | AsyncSessionTransaction
+AllSessionType = SyncSessionType | AsyncSessionType
 
 
-class DatabaseSessionFactory:
-    def __init__(self, database_url: str, app_tz: str = "UTC"):
-        self.engine = create_engine(database_url, connect_args={"options": f"-c timezone={app_tz}"})
-        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+class SyncDatabaseSessionFactory:
+    def __init__(self, database_url: str):
+        """Initializes the DatabaseSessionFactory with the given database URL.
+
+        Args:
+            database_url (str): The URL of the database to connect to.
+
+        """
+        self.engine = create_engine(database_url)
+        self.SessionLocal = sessionmaker(
+            self.engine, autocommit=False, autoflush=False, expire_on_commit=False,
+        )
 
     def get_session(self) -> Session:
+        """Creates a new database session.
+
+        Returns:
+            Session: A new SQLAlchemy session.
+
+        """
         return self.SessionLocal()
+
+
+class AsyncDatabaseSessionFactory:
+    def __init__(self, database_url: str):
+        """Initializes the AsyncDatabaseSessionFactory with the given database URL.
+
+        Args:
+            database_url (str): The URL of the database to connect to.
+
+        """
+        self.async_engine = create_async_engine(database_url)
+        self.AsyncSessionLocal = async_sessionmaker(
+            self.async_engine,
+            autocommit=False,
+            autoflush=False,
+            expire_on_commit=False,
+            class_=AsyncSession,
+        )
+
+    def get_async_session(self) -> AsyncSession:
+        """Creates a new asynchronous database session.
+
+        Returns:
+            AsyncSession: A new SQLAlchemy asynchronous session.
+
+        """
+        return self.AsyncSessionLocal()
+
+    async def close_engine(self) -> None:
+        """Closes the asynchronous database engine.
+
+        This method should be called to properly dispose of the engine and release any resources.
+        """
+        await self.async_engine.dispose()

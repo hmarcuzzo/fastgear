25062f3ecaf0e3991dc5c25c6021c70f3e7eb3f1
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2025-03-02 02:26:10 +0000

    feat: enhance utility application with synchronous and asynchronous database session middleware

--- fastutils_hmarcuzzo/applications.py
+++ fastutils_hmarcuzzo/applications.py
@@ -1,20 +1,28 @@
-from typing import List
 
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 
 from fastutils_hmarcuzzo.handlers.http_exceptions_handler import HttpExceptionsHandler
-
+from fastutils_hmarcuzzo.middlewares.db_session_middleware import (
+    AsyncDBSessionMiddleware,
+    SyncDBSessionMiddleware,
+)
 
 UTILS_CALLABLES = {
-    "http_exceptions_handler": lambda app: HttpExceptionsHandler(app),
-    "pagination": lambda app: add_pagination(app),
+    "http_exceptions_handler": lambda app, *args, **kwargs: HttpExceptionsHandler(app),
+    "http_db_session_middleware": lambda app, *args, **kwargs: app.add_middleware(
+        SyncDBSessionMiddleware, *args, **kwargs,
+    ),
+    "http_db_async_session_middleware": lambda app, *args, **kwargs: app.add_middleware(
+        AsyncDBSessionMiddleware, *args, **kwargs,
+    ),
+    "pagination": lambda app, *args, **kwargs: add_pagination(app),
 }
 
 
-def apply_utils(app: FastAPI, utils: List[str]):
+def apply_utils(app: FastAPI, utils: list[str], *args, **kwargs):
     for util in utils:
         try:
-            UTILS_CALLABLES[util](app)
+            UTILS_CALLABLES[util](app, *args, **kwargs)
         except KeyError:
             print(f"Utils {util} does not exist")

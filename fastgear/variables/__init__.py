f8e74b347fea9f6c6b8279fad7e64095a5b958a5
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2025-03-15 17:05:05 +0000

    feat: Simplify DB session management

--- fastgear/variables/__init__.py
+++
@@ -1,3 +0,0 @@
-from fastgear.variables.db_session import db_session
-
-__all__ = ["db_session"]
--- fastgear/variables/db_session.py
+++
@@ -1,6 +0,0 @@
-from contextvars import ContextVar
-
-from fastgear.common.database.sqlalchemy.session import AllSessionType
-
-# Unified context variable for both sync and async sessions
-db_session: ContextVar[AllSessionType | None] = ContextVar("db_session", default=None)

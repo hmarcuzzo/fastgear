8f67c1a68cc9c4cc6ae3a7a1a2c5b70884d3fad0
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2025-03-15 17:19:34 +0000

    refactor: Consolidate database session middleware into a single class

--- fastgear/applications.py
+++ fastgear/applications.py
@@ -4,18 +4,12 @@ from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 
 from fastgear.handlers.http_exceptions_handler import HttpExceptionsHandler
-from fastgear.middlewares.db_session_middleware import (
-    AsyncDBSessionMiddleware,
-    SyncDBSessionMiddleware,
-)
+from fastgear.middlewares.db_session_middleware import DBSessionMiddleware
 
 UTILS_CALLABLES = {
     "http_exceptions_handler": lambda app, **kwargs: HttpExceptionsHandler(app, **kwargs),
-    "http_db_sync_session_middleware": lambda app, **kwargs: app.add_middleware(
-        SyncDBSessionMiddleware, **kwargs,
-    ),
-    "http_db_async_session_middleware": lambda app, **kwargs: app.add_middleware(
-        AsyncDBSessionMiddleware, **kwargs,
+    "http_db_session_middleware": lambda app, **kwargs: app.add_middleware(
+        DBSessionMiddleware, **kwargs
     ),
     "pagination": lambda app: add_pagination(app),
 }

5517c936c068fa6e0408da855744a5567adbe588
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2025-04-30 17:15:19 +0100

    🔧 Remove DependencyInjectorUtils

--- fastgear/utils/__init__.py
+++ fastgear/utils/__init__.py
@@ -1,7 +1,6 @@
-from .dependency_injector_utils import DependencyInjectorUtils
 from .enum_utils import EnumUtils
 from .json_utils import JsonUtils
 from .logger_utils import LoggerUtils
 from .pagination_utils import PaginationUtils
 
-__all__ = ["DependencyInjectorUtils", "EnumUtils", "JsonUtils", "LoggerUtils", "PaginationUtils"]
+__all__ = ["EnumUtils", "JsonUtils", "LoggerUtils", "PaginationUtils"]

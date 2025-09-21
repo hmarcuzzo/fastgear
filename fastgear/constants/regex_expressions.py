bac1daadbab83c0f1a8f019158dd118a79791947
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2025-09-21 18:35:55 +0100

    ♻️ Update ANY_CHAR regex to include underscores

--- fastgear/constants/regex_expressions.py
+++ fastgear/constants/regex_expressions.py
@@ -2,7 +2,7 @@
 ORDER_BY_QUERY = "(ASC|DESC)$"
 
 # Regular expression to match any alphanumeric character (one or more times).
-ANY_CHAR = "^[a-zA-Z0-9]+$"
+ANY_CHAR = r"^[a-zA-Z0-9_]+$"
 
 # Regular expression to match logical operators 'AND' or 'OR'.
 OPERATOR = "(AND|OR)$"

4e8aee18a925047ff829ba6d959ff593dad824f9
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2026-04-12 14:18:14 +0100

    ♻️ Enhance import handling for BaseSchema and EntityType

--- fastgear/types/generic_types_var.py
+++ fastgear/types/generic_types_var.py
@@ -2,9 +2,13 @@ from typing import TypeVar
 
 from pydantic import BaseModel
 
-from fastgear.common.database.sqlalchemy.base import Base
+try:
+    from fastgear.common.database.sqlalchemy.base import Base
 
-EntityType = TypeVar("EntityType", bound=Base)
+    EntityType = TypeVar("EntityType", bound=Base)
+except ImportError:
+    EntityType = TypeVar("EntityType")  # type: ignore[assignment]
+
 ColumnsQueryType = TypeVar("ColumnsQueryType", bound=type[BaseModel])
 FindAllQueryType = TypeVar("FindAllQueryType", bound=type[BaseModel])
 OrderByQueryType = TypeVar("OrderByQueryType", bound=type[BaseModel])
